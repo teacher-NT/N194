f964a9bd3e2d7205fc6f1a8169ac2eb2391128d7
Teacher NT <holmonbek@gmail.com>
2026-03-31 14:50:30 +0500

    Dict

--- main.py
+++ main.py
@@ -9,7 +9,43 @@ car = {
     "max_speed": 200,
 }
 
-if "model" in car:
-    print(car["model"])
-else:
-    print("No")
+# print(car.get("mode", "Bunday kalit yo'q"))
+# print(car["mode"])
+
+# print(car.keys())
+# print(car.values())
+
+# m = car.pop("yil")
+# print(m)
+# print(car)
+
+
+# for k, q in car.items():
+#     print(k, q)
+
+# n = car.copy()
+# n['model'] = "Damas"
+# print(n, car)
+
+# car.clear()
+# print(car)
+
+# k = car.popitem()
+# print(k)
+# print(car)
+
+car.update({"yil": 2023,"narx":150000, "rang":"Qora"})
+# car['yil'] = 2023
+# car["narx"] = 150000
+# car['rang'] = "Qora"
+print(car)
+
+
+# for i in car:
+#     print(i, car[i])
+
+
+# if "model" in car:
+#     print(car["model"])
+# else:
+#     print("No")
