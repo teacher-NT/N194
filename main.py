7f9c8a209fb7eeee76cb773fbf6ef69f9cc686cf
Teacher NT <holmonbek@gmail.com>
2026-03-31 14:26:45 +0500

    Dict

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
 import os
 os.system("cls")
 
-set1 = {1,2,3,4,5,6}
-set2 = {4,5,6,7,8,9}
+car = {
+    "brand": "Mers",
+    "model": "Gelik",
+    "narx": 200_000,
+    "yil": 2026,
+    "max_speed": 200,
+}
 
-# set3 = set1.intersection(set2)
-# print(set3)
+# car["rang"] = "Oq"
 
-# set1.intersection_update(set2)
-# print(set1)
+# print(car["brand"])
+# print(car["narx"])
+# print(car)
 
-# set3 = set1.difference(set2)
-# print(set3)
-
-# set1.difference_update(set2)
-# print(set1)
-
-# set3 = set1.symmetric_difference(set2)
-# print(set3)
-
-# set1.symmetric_difference_update(set2)
-# print(set1)
-
-set1 = {1,2,3,4,5,6,7,8,9,10}
-set2 = {3,7,4}
-# print(set2.issubset(set1))
-print(set1.issuperset(set2))
-
-
-
+# print(car["brend"])
