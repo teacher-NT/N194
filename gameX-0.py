e394f29b5671ca69bf83c6792f678a3e5deea6fe
Teacher NT <holmonbek@gmail.com>
2026-04-04 15:32:02 +0500

    Game

---
+++ gameX-0.py
@@ -0,0 +1,88 @@
+from random import choice
+import os
+os.system("cls")
+
+def bosh_doska_hosil_qil():
+   return [
+       '1','2','3','4','5','6','7','8','9'
+   ]
+    
+
+def doskani_korsat(doska):
+    start = 0
+    end = 3
+    print("-------------")
+    for i in range(3):
+        print("|"," | ".join(doska[start:end]), "|")
+        print("-------------")
+        start += 3
+        end  += 3
+
+def foydalanuvchi_tanlasin(doska):
+    while True:
+        foy = input("Sizni galiniz: ")
+        if foy in doska:
+            index = doska.index(foy)
+            doska[index] = 'O'
+            break
+        else:
+            print("Iltimos bo'sh katakni tanlang!")
+
+
+
+def bosh_maydonlar(doska):
+    free = []
+    for i in doska:
+        if i != 'O' and i != 'X':
+            free.append(i)
+    return free
+    
+    
+def golib_bormi(doska, belgi):
+    if doska[0] == belgi and doska[1]==belgi and doska[2]==belgi:
+        return True
+    elif doska[0] == belgi and doska[3] == belgi and doska[6] == belgi:
+        return True 
+    elif doska[0] == belgi and doska[4] == belgi and doska[8] == belgi:
+        return True 
+    elif doska[1] == belgi and doska[4] == belgi and doska[7] == belgi :
+        return True
+    elif doska[2] == belgi and doska[5] == belgi and doska[8] == belgi :
+        return True
+    elif doska[3] == belgi and doska[4] == belgi and doska[5] == belgi :
+        return True
+    elif doska[6] == belgi and doska[7] == belgi and doska[8] == belgi :
+        return True
+    elif doska[2] == belgi and doska[4] == belgi and doska[6] == belgi :
+        return True
+    else:
+        return False
+
+    
+def kompyuter_tanlasin(doska):
+    free = bosh_maydonlar((doska))
+    if not free:
+        return
+    ch = choice(free)
+    index = doska.index(ch)
+    doska[index] = 'X'
+
+
+doska = bosh_doska_hosil_qil()
+while bosh_maydonlar(doska):
+    doskani_korsat(doska)
+    foydalanuvchi_tanlasin(doska)
+    if golib_bormi(doska, 'O'):
+        print("Siz g'olib bo'ldingiz!!!")
+        break
+    kompyuter_tanlasin(doska)
+    if golib_bormi(doska, 'X'):
+        doskani_korsat(doska)
+        print("Kompyuter yutdi!!!")
+        break
+else:
+    print("G'olib aniqlanmadi...")
+
+
+
+
--- main.py
+++ main.py
@@ -1,51 +1,5 @@
 import os
-os.system("cls")
-
-car = {
-    "brand": "Mers",
-    "model": "Gelik",
-    "narx": 200_000,
-    "yil": 2026,
-    "max_speed": 200,
-}
-
-# print(car.get("mode", "Bunday kalit yo'q"))
-# print(car["mode"])
-
-# print(car.keys())
-# print(car.values())
-
-# m = car.pop("yil")
-# print(m)
-# print(car)
+os.system('cls')
 
 
-# for k, q in car.items():
-#     print(k, q)
 
-# n = car.copy()
-# n['model'] = "Damas"
-# print(n, car)
-
-# car.clear()
-# print(car)
-
-# k = car.popitem()
-# print(k)
-# print(car)
-
-car.update({"yil": 2023,"narx":150000, "rang":"Qora"})
-# car['yil'] = 2023
-# car["narx"] = 150000
-# car['rang'] = "Qora"
-print(car)
-
-
-# for i in car:
-#     print(i, car[i])
-
-
-# if "model" in car:
-#     print(car["model"])
-# else:
-#     print("No")
