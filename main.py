7ce423156bb5bf849306e048d731f28d5277d623
Teacher NT <holmonbek@gmail.com>
2026-04-28 16:11:57 +0500

    PyQt5 3

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ window.setGeometry(1200, 200, 400, 600)
 window.setWindowTitle("Bu birinchi ilovam")
 
 matn1 = QLabel(window)
-matn1.setText("Python dasturidan salom")
+matn1.setText("Counter")
 # matn1.setGeometry(20,50,300,200)
 matn1.setStyleSheet("""font-size:28px; 
                     margin-top:50px;
@@ -25,6 +25,18 @@ matn1.setStyleSheet("""font-size:28px;
                     color:red;
                     font-weight: bold;""")
 # matn1.setFont(font1)
+
+matn2 = QLabel(window)
+matn2.setText("0")
+matn2.setStyleSheet("""
+                    font-size: 60px;
+                    color: blue;
+                    margin-top:170px;
+                    margin-left:170px;
+""")
+matn2.setFixedWidth(600)
+
+
 style1 = """
     font-size: 18px;
     color: blue;
@@ -33,29 +45,35 @@ style1 = """
     border: 4px solid cyan;
 """
 
+style2 = """
+    font-size: 18px;
+    color: blue;
+    background-color: red;
+    border-radius: 20px;
+    border: 4px solid cyan;
+"""
+
 def func_btn1():
-    global matn1
-    matn1.setText("Salom Ibrohim!!!")
+    global matn2
+    n = int(matn2.text())+1
+    matn2.setText(str(n))
+
 
 button1 = QPushButton(window)
-button1.setText("Start")
-button1.setGeometry(80, 150, 100, 50)
+button1.setText("Count")
+button1.setGeometry(80, 300, 100, 50)
 button1.setStyleSheet(style1)
 button1.clicked.connect(func_btn1)
 
-color = "white"
+
 def func_btn2():
-    global color
-    if color == "white":
-        color = 'black'
-    else:
-        color = 'white'
-    window.setStyleSheet(f"background-color:{color};")
+    global matn2
+    matn2.setText("0")
 
 button2 = QPushButton(window)
-button2.setText("Stop")
-button2.setGeometry(250, 150, 100, 50)
-button2.setStyleSheet(style1)
+button2.setText("Reset")
+button2.setGeometry(250, 300, 100, 50)
+button2.setStyleSheet(style2)
 button2.clicked.connect(func_btn2)
 
 window.show()
