95fb78115757e250104e150e09d09c9b517cfe80
Teacher NT <holmonbek@gmail.com>
2026-04-28 14:50:43 +0500

    PyQt5 1

--- main.py
+++ main.py
@@ -1,41 +1,13 @@
 import os
 os.system('cls')
 
-from abc import ABC, abstractmethod
+from PyQt5.QtWidgets import QApplication, QWidget
 
-class Player(ABC):
-    @abstractmethod
-    def run(self):
-        pass
+app = QApplication([])
 
-    @abstractmethod
-    def jump(self):
-        pass
+window = QWidget()
+window.setGeometry(1200, 200, 400, 600)
+window.setWindowTitle("Bu birinchi ilovam")
+window.show()
 
-    @abstractmethod
-    def kick(self):
-        pass
-
-
-class Vector(Player):
-    def __init__(self, ball, heart):
-        self.ball = ball
-        self.heart = heart
-    
-    def run(self):
-        print("Vector is running...")
-    
-    def kick(self):
-        print("Vector is kicking...")
-    
-    def jump(self):
-        print("Vector is jumping...")
-
-    def fly(self):
-        print("Vector is flying...")
-
-v1 = Vector(100, 5)
-v1.jump()
-v1.run()
-v1.kick()
-v1.fly()
+app.exec_()
