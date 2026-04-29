da1f66450fc686cec23a1b5960fb8f5780dfcc48
Teacher NT <holmonbek@gmail.com>
2026-04-29 13:29:46 +0500

    Traffic light

--- main.py
+++ main.py
@@ -2,61 +2,116 @@ import os
 os.system('cls')
 
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QLabel, QPushButton,
-    QLineEdit
+    QApplication, QWidget, QPushButton, QLabel
 )
-from PyQt5.QtGui import QFont
 
-app = QApplication([])
+from PyQt5.QtCore import QTimer
 
-window = QWidget()
-window.setGeometry(1200, 200, 400, 600)
-window.setWindowTitle("Bu birinchi ilovam")
+app  = QApplication([])
+win = QWidget()
+win.setWindowTitle("Svetafor")
+win.setGeometry(1200, 80, 400, 900)
+win.setStyleSheet("""
+    background-color: black;
+""")
 
-matn1 = QLabel(window)
-matn1.setText("Counter")
-# matn1.setGeometry(20,50,300,200)
-matn1.setStyleSheet("""font-size:28px; 
-                    margin-top:50px;
-                    margin-left:20px; 
-                    color:red;
-                    font-weight: bold;""")
-
-in1 = QLineEdit(window)
-in1.setGeometry(70,100, 280, 50)
-in1.setStyleSheet("""font-size:25px;
-                  border-radius:20px;
-                  border: 2px solid black""")
-in1.setPlaceholderText("rang kiriting...")
-
-style1 = """
-    font-size: 18px;
-    color: blue;
-    background-color: lime;
-    border-radius: 20px;
-    border: 4px solid cyan;
-"""
-
-style2 = """
-    font-size: 18px;
-    color: blue;
-    background-color: red;
-    border-radius: 20px;
-    border: 4px solid cyan;
-"""
-
-def func_btn1():
-    global in1
-    color = in1.text()
-    window.setStyleSheet(f"background-color: {color};")
-    
+btn1 = QPushButton(win)
+btn1.setGeometry(75, 50, 250,250)
+btn1.setStyleSheet("""
+    background-color: white;
+                   border:  5px solid blue;
+                   border-radius: 125;
+""")
 
 
-button1 = QPushButton(window)
-button1.setText("Count")
-button1.setGeometry(80, 300, 100, 50)
-button1.setStyleSheet(style1)
-button1.clicked.connect(func_btn1)
+btn2 = QPushButton(win)
+btn2.setGeometry(75, 310, 250,250)
+btn2.setStyleSheet("""
+    background-color: white;
+                   border:  5px solid blue;
+                   border-radius: 125;
+""")
 
-window.show()
+
+btn3 = QPushButton(win)
+btn3.setGeometry(75, 570, 250,250)
+btn3.setStyleSheet("""
+    background-color: white;
+                   border:  5px solid blue;
+                   border-radius: 125;
+""")
+
+seconds = 23
+def func1():
+    global seconds
+    if 13 < seconds <= 23:
+        btn1.setStyleSheet("""
+            background-color: red;
+                        border:  5px solid blue;
+                        border-radius: 125;
+                        font-size: 70px;
+                        color: white;
+        """)
+        n = seconds - 13
+        btn1.setText(str(n))
+        btn2.setStyleSheet("""
+            background-color: black;
+                        border:  5px solid blue;
+                        border-radius: 125;
+        """)
+        btn3.setStyleSheet("""
+            background-color: black;
+                        border:  5px solid blue;
+                        border-radius: 125;
+        """)
+    elif 10 < seconds <= 13:
+        btn1.setStyleSheet("""
+            background-color: black;
+                        border:  5px solid blue;
+                        border-radius: 125;
+        """)
+        btn2.setStyleSheet("""
+            background-color: yellow;
+                        border:  5px solid blue;
+                        border-radius: 125;
+                        font-size: 70px;
+                        color: black;
+        """)
+        n = seconds - 10
+        btn2.setText(str(n))
+        btn3.setStyleSheet("""
+            background-color: black;
+                        border:  5px solid blue;
+                        border-radius: 125;
+        """)
+    elif 0 < seconds <= 10:
+        btn1.setStyleSheet("""
+            background-color: black;
+                        border:  5px solid blue;
+                        border-radius: 125;
+        """)
+        btn2.setStyleSheet("""
+            background-color: black;
+                        border:  5px solid blue;
+                        border-radius: 125;
+        """)
+        btn3.setStyleSheet("""
+            background-color: lime;
+                        border:  5px solid blue;
+                        border-radius: 125;
+                        font-size: 70px;
+                        color: white;
+        """)
+        btn3.setText(str(seconds))
+    elif seconds <= 0:
+        seconds = 23
+    seconds -= 1
+
+ 
+timer = QTimer()
+timer.timeout.connect(func1)
+timer.start(1000)
+
+
+win.show()
 app.exec_()
