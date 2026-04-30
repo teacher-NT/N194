4095ceadaa65acb8ffb287a2c97172018b52a91c
Teacher NT <holmonbek@gmail.com>
2026-04-30 15:43:05 +0500

    Checkbox

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ os.system('cls')
 
 from PyQt5.QtWidgets import (
     QApplication, QWidget, QLabel, QPushButton,
-    QVBoxLayout, QHBoxLayout, QComboBox
+    QVBoxLayout, QHBoxLayout, QComboBox, QCheckBox
 )
 
 body_style = """
@@ -24,6 +24,10 @@ menu_style = """
     color: #6044bd;
 """
 
+check_tyle = """
+    font-size: 20px;
+"""
+
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -37,19 +41,60 @@ class MyWindow(QWidget):
         self.label2 = QLabel("Savat: bo'sh")
         self.label2.setStyleSheet(label2_style)
         self.vbox.addWidget(self.label2)
+        self.label3 = QLabel("Ichimliklar: bo'sh")
+        self.label3.setStyleSheet(label2_style)
+        self.vbox.addWidget(self.label3)
+        
         
         self.menu = QComboBox()
         self.menu.addItems(['Somsa', 'Qazi', 'Osh', 'Manti', 'Qozon kabob', 'Shashlik', 'Beshbarmoq'])
         self.vbox.addWidget(self.menu)
         self.menu.setStyleSheet(menu_style)
         self.menu.currentTextChanged.connect(self.get_food)
+        self.add_checkbox()
         self.setLayout(self.vbox)
         self.show()
     
     def get_food(self):
         current = self.menu.currentText()
         self.label2.setText(f"Savat: {current}")
+
+    def add_checkbox(self):
+        self.check1 = QCheckBox("Choy")
+        self.vbox.addWidget(self.check1)
+        self.check1.setStyleSheet(check_tyle)
+        self.check1.stateChanged.connect(self.check_func)
+
+        self.check2 = QCheckBox("Kofe")
+        self.vbox.addWidget(self.check2)
+        self.check2.setStyleSheet(check_tyle)
+        self.check2.stateChanged.connect(self.check_func)
+
+
+        self.check3 = QCheckBox("Mineral Suv")
+        self.vbox.addWidget(self.check3)
+        self.check3.setStyleSheet(check_tyle)
+        self.check3.stateChanged.connect(self.check_func)
+
+
+        self.check4 = QCheckBox("Sut")
+        self.vbox.addWidget(self.check4)
+        self.check4.setStyleSheet(check_tyle)
+        self.check4.stateChanged.connect(self.check_func)
+
+
+    def check_func(self):
+        result = "Ichimliklar: "
+        if self.check1.isChecked():
+            result += f"{self.check1.text()}, "
+        if self.check2.isChecked():
+            result += f"{self.check2.text()}, "
+        if self.check3.isChecked():
+            result += f"{self.check3.text()}, "
+        if self.check4.isChecked():
+            result += f"{self.check4.text()}, "
         
+        self.label3.setText(result)
 
 
 app = QApplication([])
