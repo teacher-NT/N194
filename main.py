606c0cf2a92b16523406026cb51cbefc638c51c5
Teacher NT <holmonbek@gmail.com>
2026-04-30 14:35:03 +0500

    clean code

--- main.py
+++ main.py
@@ -2,116 +2,28 @@ import os
 os.system('cls')
 
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QPushButton, QLabel
+    QApplication, QWidget, QLabel, QPushButton
 )
 
-from PyQt5.QtCore import QTimer
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(400, 700)
+        self.label1 = QLabel(self)
+        self.label1.setText("Salom Dunyo")
+        self.label1.move(50, 50)
+        self.btn1 = QPushButton(self)
+        self.btn1.setText("Tugmacha")
+        self.btn1.move(50, 150)
+        self.btn1.clicked.connect(self.func1)
 
-app  = QApplication([])
-win = QWidget()
-win.setWindowTitle("Svetafor")
-win.setGeometry(1200, 80, 400, 900)
-win.setStyleSheet("""
-    background-color: black;
-""")
-
-btn1 = QPushButton(win)
-btn1.setGeometry(75, 50, 250,250)
-btn1.setStyleSheet("""
-    background-color: white;
-                   border:  5px solid blue;
-                   border-radius: 125;
-""")
-
-
-btn2 = QPushButton(win)
-btn2.setGeometry(75, 310, 250,250)
-btn2.setStyleSheet("""
-    background-color: white;
-                   border:  5px solid blue;
-                   border-radius: 125;
-""")
+        self.show()
+    
+    def func1(self):
+        self.label1.setText("Tuugmacha bosildi...")
 
 
-btn3 = QPushButton(win)
-btn3.setGeometry(75, 570, 250,250)
-btn3.setStyleSheet("""
-    background-color: white;
-                   border:  5px solid blue;
-                   border-radius: 125;
-""")
-
-seconds = 23
-def func1():
-    global seconds
-    if 13 < seconds <= 23:
-        btn1.setStyleSheet("""
-            background-color: red;
-                        border:  5px solid blue;
-                        border-radius: 125;
-                        font-size: 70px;
-                        color: white;
-        """)
-        n = seconds - 13
-        btn1.setText(str(n))
-        btn2.setStyleSheet("""
-            background-color: black;
-                        border:  5px solid blue;
-                        border-radius: 125;
-        """)
-        btn3.setStyleSheet("""
-            background-color: black;
-                        border:  5px solid blue;
-                        border-radius: 125;
-        """)
-    elif 10 < seconds <= 13:
-        btn1.setStyleSheet("""
-            background-color: black;
-                        border:  5px solid blue;
-                        border-radius: 125;
-        """)
-        btn2.setStyleSheet("""
-            background-color: yellow;
-                        border:  5px solid blue;
-                        border-radius: 125;
-                        font-size: 70px;
-                        color: black;
-        """)
-        n = seconds - 10
-        btn2.setText(str(n))
-        btn3.setStyleSheet("""
-            background-color: black;
-                        border:  5px solid blue;
-                        border-radius: 125;
-        """)
-    elif 0 < seconds <= 10:
-        btn1.setStyleSheet("""
-            background-color: black;
-                        border:  5px solid blue;
-                        border-radius: 125;
-        """)
-        btn2.setStyleSheet("""
-            background-color: black;
-                        border:  5px solid blue;
-                        border-radius: 125;
-        """)
-        btn3.setStyleSheet("""
-            background-color: lime;
-                        border:  5px solid blue;
-                        border-radius: 125;
-                        font-size: 70px;
-                        color: white;
-        """)
-        btn3.setText(str(seconds))
-    elif seconds <= 0:
-        seconds = 23
-    seconds -= 1
-
- 
-timer = QTimer()
-timer.timeout.connect(func1)
-timer.start(1000)
-
-
-win.show()
+app = QApplication([])
+win = MyWindow()
 app.exec_()
+
