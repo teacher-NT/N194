3fdf0fef0da8736c51535831f9475c035aa54db3
Teacher NT <holmonbek@gmail.com>
2026-04-30 14:51:02 +0500

    Layout

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import os
 os.system('cls')
 
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QLabel, QPushButton
+    QApplication, QWidget, QLabel, QPushButton,
+    QVBoxLayout, QHBoxLayout
 )
 
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setFixedSize(400, 700)
-        self.label1 = QLabel(self)
-        self.label1.setText("Salom Dunyo")
-        self.label1.move(50, 50)
-        self.btn1 = QPushButton(self)
-        self.btn1.setText("Tugmacha")
-        self.btn1.move(50, 150)
-        self.btn1.clicked.connect(self.func1)
+        self.vbox = QHBoxLayout()
+        # self.setFixedSize(400, 700)
+        self.label1 = QLabel("Salom Dunyo")
+        self.vbox.addWidget(self.label1)
+        self.btn1 = QPushButton("Tugmacha 1")
 
+        self.vbox.addWidget(self.btn1)
+        self.btn1.clicked.connect(self.func1)
+        self.btn2 = QPushButton("Tugmacha 2")
+        self.vbox.addWidget(self.btn2)
+
+        self.btn3 = QPushButton("Tugmacha 3")
+        self.vbox.addWidget(self.btn3)
+
+        self.setLayout(self.vbox)
         self.show()
     
     def func1(self):
