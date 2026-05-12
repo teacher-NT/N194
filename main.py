c77f240bc9a6e8f606fe169fc1d16c5ef934fd70
Teacher NT <holmonbek@gmail.com>
2026-05-12 15:44:39 +0500

    insert

--- main.py
+++ main.py
@@ -12,22 +12,27 @@ mydb = mysql.connector.connect(
 
 cursor = mydb.cursor()
 
-_id = input("Id: ")
-username = input("Username: ")
-first = input("First name: ")
-last = input("Last name: ")
-posts = int(input("Posts count: "))
-followers = int(input("Followers: "))
-following = int(input("Followings: "))
-joined = input("Joined date: ")
+# _id = input("Id: ")
+# username = input("Username: ")
+# first = input("First name: ")
+# last = input("Last name: ")
+# posts = int(input("Posts count: "))
+# followers = int(input("Followers: "))
+# following = int(input("Followings: "))
+# joined = input("Joined date: ")
 
-sql = f"""
-    INSERT INTO users(user_id,username,first_name,last_name,posts_count,followers,followings,joined_date)
-    VALUES
-    ({_id}, '{username}', '{first}', '{last}', {posts}, {followers}, {following}, '{joined}');
+# sql = f"""
+#     INSERT INTO users(user_id,username,first_name,last_name,posts_count,followers,followings,joined_date)
+#     VALUES
+#     ({_id}, '{username}', '{first}', '{last}', {posts}, {followers}, {following}, '{joined}');
+# """
+
+
+sql2 = """
+    UPDATE users SET followers = 7000000
+    WHERE user_id = 31;
 """
-
-cursor.execute(sql)
+cursor.execute(sql2)
 mydb.commit()
 
 print(f"{cursor.rowcount} rows affected")
